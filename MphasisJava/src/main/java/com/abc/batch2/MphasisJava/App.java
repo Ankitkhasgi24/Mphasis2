8d61785444509794a563a01c6eb842138ca37c39
Nithya <Nithya@LAPTOP-LTLBT54P>
2019-06-24 12:06:47 +0530

    THIS IS EMPHASIS BATCH2

---
+++ MphasisJava/src/main/java/com/abc/batch2/MphasisJava/App.java
@@ -0,0 +1,13 @@
+package com.abc.batch2.MphasisJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MphasisJava/src/main/java/com/abc/batch2/MphasisJava/Navigate.java
@@ -0,0 +1,23 @@
+package com.abc.batch2.MphasisJava;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigate {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	driver.get("https://www.gmail.com");
+		
+		
+		
+	
+	
+	
+	
+	
+	}
+
+}
